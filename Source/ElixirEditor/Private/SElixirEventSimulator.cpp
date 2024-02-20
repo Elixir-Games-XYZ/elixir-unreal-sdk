063039c55c05b04b9a81304cc775b87cf73fd784
Danny Povolotski <dannypovolotski@gmail.com>
2024-02-21 00:24:10 +0300

    Mac & Linux support (#16)
    * Overlay messages support
    
    * compatibility with unreal 5.2+ and logout
    
    * add elixir menu to the menubar
    
    * Improved simulator window UI
    
    * add tournaments
    
    * Updated Raven
    
    * Feat: Mac and Linux support
    
    ---------
    
    Co-authored-by: Sergey Makovkin <makovkin.s@gmail.com>

--- Source/ElixirEditor/Private/SElixirEventSimulator.cpp
+++ Source/ElixirEditor/Private/SElixirEventSimulator.cpp
@@ -21,15 +21,19 @@ using namespace elixir::overlay::message;
 class STableItemWidget : public SMultiColumnTableRow<TSharedPtr<FElixirEventItem>>
 {
 public:
-	SLATE_BEGIN_ARGS(STableItemWidget) {}
+	SLATE_BEGIN_ARGS(STableItemWidget)
+		{
+		}
+
 	SLATE_END_ARGS()
 
-	void Construct(const FArguments& InArgs, const TSharedRef<STableViewBase>& InOwnerTableView, TSharedPtr<FElixirEventItem> InItem)
+	void Construct(const FArguments& InArgs, const TSharedRef<STableViewBase>& InOwnerTableView,
+	               TSharedPtr<FElixirEventItem> InItem)
 	{
 		ItemWeakPtr = InItem;
 		SMultiColumnTableRow::Construct(FSuperRowType::FArguments(), InOwnerTableView);
 	}
-	
+
 	virtual TSharedRef<SWidget> GenerateWidgetForColumn(const FName& ColumnName) override
 	{
 		if (TSharedPtr<FElixirEventItem> Item = ItemWeakPtr.Pin())
@@ -38,21 +42,21 @@ public:
 			{
 				return SNew(STextBlock).Text(FText::FromString(Item->Direction));
 			}
-			else if (ColumnName == "Name")
+			if (ColumnName == "Name")
 			{
 				return SNew(STextBlock).Text(FText::FromString(Item->Name));
 			}
-			else if (ColumnName == "Payload")
+			if (ColumnName == "Payload")
 			{
 				return SNew(STextBlock).Text(FText::FromString(Item->Payload));
-			}			
+			}
 		}
 
 		return SNullWidget::NullWidget;
 	}
 
 private:
-	TWeakPtr<FElixirEventItem> ItemWeakPtr;	
+	TWeakPtr<FElixirEventItem> ItemWeakPtr;
 };
 
 SElixirEventSimulator::SElixirEventSimulator()
@@ -78,186 +82,195 @@ void SElixirEventSimulator::Construct(const FArguments& InArgs)
 	EventTypes.Add(MakeShareable(new FString("CheckoutResult")));
 	EventTypes.Add(MakeShareable(new FString("OpenStateChange")));
 	SelectedEventType = "CheckoutResult";
-	
+
 	EventsListView = SNew(SListView<TSharedPtr<FElixirEventItem>>)
 		.ItemHeight(24.0f)
 		.ListItemsSource(&Events)
 		.OnGenerateRow_Lambda([](TSharedPtr<FElixirEventItem> Item, const TSharedRef<STableViewBase>& OwnerTable)
-		{
-			return SNew(STableItemWidget, OwnerTable, Item);
-		})
+	                                                              {
+		                                                              return SNew(STableItemWidget, OwnerTable, Item);
+	                                                              })
 		.HeaderRow
-		(
-			SNew(SHeaderRow)
-			+ SHeaderRow::Column("Direction").DefaultLabel(FText::FromString("Direction"))
-			.FillWidth(0.2f)
-			+ SHeaderRow::Column("Name").DefaultLabel(FText::FromString("Event Name"))
-			.FillWidth(0.4f)
-			+ SHeaderRow::Column("Payload").DefaultLabel(FText::FromString("Payload"))
-			.FillWidth(0.4f)
-		);
+	                                                              (
+		                                                              SNew(SHeaderRow)
+		                                                              + SHeaderRow::Column("Direction").DefaultLabel(
+			                                                              FText::FromString("Direction"))
+		                                                              .FillWidth(0.2f)
+		                                                              + SHeaderRow::Column("Name").DefaultLabel(
+			                                                              FText::FromString("Event Name"))
+		                                                              .FillWidth(0.4f)
+		                                                              + SHeaderRow::Column("Payload").DefaultLabel(
+			                                                              FText::FromString("Payload"))
+		                                                              .FillWidth(0.4f)
+	                                                              );
 
 	EventTypeComboBox = SNew(SComboBox<TSharedPtr<FString>>)
 		.OptionsSource(&EventTypes)
 		.OnSelectionChanged_Lambda([this](TSharedPtr<FString> Value, ESelectInfo::Type SelectInfo)
-		{
-			SelectedEventType = *Value;
-			if (SelectedEventType == "CheckoutResult")
-			{
-				SuccessHorizontalBox->SetVisibility(EVisibility::Visible);
-				SkuHorizontalBox->SetVisibility(EVisibility::Visible);
-				IsOpenHorizontalBox->SetVisibility(EVisibility::Collapsed);
-			}
-			else if (SelectedEventType == "OpenStateChange")
-			{
-				SuccessHorizontalBox->SetVisibility(EVisibility::Collapsed);
-				SkuHorizontalBox->SetVisibility(EVisibility::Collapsed);
-				IsOpenHorizontalBox->SetVisibility(EVisibility::Visible);
-			}
-		})
+	                                                        {
+		                                                        SelectedEventType = *Value;
+		                                                        if (SelectedEventType == "CheckoutResult")
+		                                                        {
+			                                                        SuccessHorizontalBox->SetVisibility(
+				                                                        EVisibility::Visible);
+			                                                        SkuHorizontalBox->SetVisibility(
+				                                                        EVisibility::Visible);
+			                                                        IsOpenHorizontalBox->SetVisibility(
+				                                                        EVisibility::Collapsed);
+		                                                        }
+		                                                        else if (SelectedEventType == "OpenStateChange")
+		                                                        {
+			                                                        SuccessHorizontalBox->SetVisibility(
+				                                                        EVisibility::Collapsed);
+			                                                        SkuHorizontalBox->SetVisibility(
+				                                                        EVisibility::Collapsed);
+			                                                        IsOpenHorizontalBox->SetVisibility(
+				                                                        EVisibility::Visible);
+		                                                        }
+	                                                        })
 		.OnGenerateWidget_Lambda([](TSharedPtr<FString> Value)
+	                                                        {
+		                                                        return SNew(STextBlock).Text(FText::FromString(*Value));
+	                                                        })
+	[
+		SNew(STextBlock).Text_Lambda([this]
 		{
-			return SNew(STextBlock).Text(FText::FromString(*Value));
+			return FText::FromString(SelectedEventType);
 		})
-		[
-			SNew(STextBlock).Text_Lambda([this]
-			{
-				return FText::FromString(SelectedEventType);
-			})
-		];
+	];
 
-	
+
 	SuccessHorizontalBox = SNew(SHorizontalBox)
-	+ SHorizontalBox::Slot()
-	.MaxWidth(100)
-	[
-		SNew(STextBlock).Text(FText::FromString("Success"))
-	]
-	+ SHorizontalBox::Slot()
-	.FillWidth(1.0)
-	[
-		SAssignNew(SuccessCheckBox, SCheckBox)
-	];
-	
-	SkuHorizontalBox = SNew(SHorizontalBox)
-	+ SHorizontalBox::Slot()
-	.MaxWidth(100)
-	[
-		SNew(STextBlock).Text(FText::FromString("Sku"))
-	]
-	+ SHorizontalBox::Slot()
-	.FillWidth(1.0)
-	[
-		SAssignNew(SkuEditableTextBox, SEditableTextBox)
-	];
-	
-	IsOpenHorizontalBox = SNew(SHorizontalBox)
-	.Visibility(EVisibility::Collapsed)
-	+ SHorizontalBox::Slot()
-	.MaxWidth(100)
-	[
-		SNew(STextBlock).Text(FText::FromString("IsOpen"))
-	]
-	+ SHorizontalBox::Slot()
-	.FillWidth(1.0)
-	[
-		SAssignNew(IsOpenCheckBox, SCheckBox)
-	];	
-	
-	TSharedPtr<SVerticalBox> VerticalBox = SNew(SVerticalBox)
-	+ SVerticalBox::Slot()
-	.AutoHeight()
-	[
-		SNew(SHorizontalBox)
-		+ SHorizontalBox::Slot()
-		.Padding(0.0f, 3.0f, 0.0f, 3.0f)
-		.AutoWidth()
-		[
-			SNew(SButton)
-		   .HAlign(HAlign_Center)
-		   .OnClicked(this, &SElixirEventSimulator::OnStartStop)
-		   .Content()
-		   [
-			   SNew(STextBlock)
-			   .MinDesiredWidth(100)
-			   .Text(this, &SElixirEventSimulator::GetStartStopButtonText)
-		   ]
-		]
-		+ SHorizontalBox::Slot()
-		.Padding(6.0f, 3.0f, 0.0f, 3.0f)
-		.AutoWidth()
-		[
-			SNew(SButton)
-			.HAlign(HAlign_Center)
-			.OnClicked(this, &SElixirEventSimulator::OnClear)
-			.Content()
-			[
-				SNew(STextBlock)
-				.MinDesiredWidth(100)
-				.Text(FText::FromString("Clear"))
-            ]
-		]
-	]
-	+ SVerticalBox::Slot()
-	.FillHeight(1.0f)
-	.Padding(0.0f, 3.0f, 0.0f, 3.0f)
-	[
-		SNew(SBorder)
-		.Padding(0)
-		.BorderImage(FCoreStyle::Get().GetBrush("ToolPanel.GroupBorder"))
-		[
-			EventsListView.ToSharedRef()
-		]		
-	]
-	+ SVerticalBox::Slot()
-	.AutoHeight()
-	.Padding(0.0f, 3.0f, 0.0f, 3.0f)
-	[
-		SNew(SHorizontalBox)
 		+ SHorizontalBox::Slot()
 		.MaxWidth(100)
 		[
-			SNew(STextBlock).Text(FText::FromString("Event Type"))
+			SNew(STextBlock).Text(FText::FromString("Success"))
 		]
 		+ SHorizontalBox::Slot()
 		.FillWidth(1.0)
 		[
-			EventTypeComboBox.ToSharedRef()
+			SAssignNew(SuccessCheckBox, SCheckBox)
+		];
+
+	SkuHorizontalBox = SNew(SHorizontalBox)
+		+ SHorizontalBox::Slot()
+		.MaxWidth(100)
+		[
+			SNew(STextBlock).Text(FText::FromString("Sku"))
 		]
-	]
-	+ SVerticalBox::Slot()
-	.AutoHeight()
-	.Padding(0.0f, 3.0f, 0.0f, 3.0f)
-	[
-		SuccessHorizontalBox.ToSharedRef()
-	]
-	+ SVerticalBox::Slot()
-	.AutoHeight()
-	.Padding(0.0f, 3.0f, 0.0f, 3.0f)
-	[
-		SkuHorizontalBox.ToSharedRef()
-	]	
-	+ SVerticalBox::Slot()
-	.AutoHeight()
-	.Padding(0.0f, 3.0f, 0.0f, 3.0f)
-	[
-		IsOpenHorizontalBox.ToSharedRef()
-	]
-	+ SVerticalBox::Slot()
-	.AutoHeight()
-	.Padding(0, 10.0f, 0, 3.0f)
-	[
-		SNew(SButton)
+		+ SHorizontalBox::Slot()
+		.FillWidth(1.0)
+		[
+			SAssignNew(SkuEditableTextBox, SEditableTextBox)
+		];
+
+	IsOpenHorizontalBox = SNew(SHorizontalBox)
+		.Visibility(EVisibility::Collapsed)
+		+ SHorizontalBox::Slot()
+		.MaxWidth(100)
+		[
+			SNew(STextBlock).Text(FText::FromString("IsOpen"))
+		]
+		+ SHorizontalBox::Slot()
+		.FillWidth(1.0)
+		[
+			SAssignNew(IsOpenCheckBox, SCheckBox)
+		];
+
+	TSharedPtr<SVerticalBox> VerticalBox = SNew(SVerticalBox)
+		+ SVerticalBox::Slot()
+		.AutoHeight()
+		[
+			SNew(SHorizontalBox)
+			+ SHorizontalBox::Slot()
+			  .Padding(0.0f, 3.0f, 0.0f, 3.0f)
+			  .AutoWidth()
+			[
+				SNew(SButton)
+		   .HAlign(HAlign_Center)
+		   .OnClicked(this, &SElixirEventSimulator::OnStartStop)
+		   .Content()
+				[
+					SNew(STextBlock)
+			   .MinDesiredWidth(100)
+			   .Text(this, &SElixirEventSimulator::GetStartStopButtonText)
+				]
+			]
+			+ SHorizontalBox::Slot()
+			  .Padding(6.0f, 3.0f, 0.0f, 3.0f)
+			  .AutoWidth()
+			[
+				SNew(SButton)
+			.HAlign(HAlign_Center)
+			.OnClicked(this, &SElixirEventSimulator::OnClear)
+			.Content()
+				[
+					SNew(STextBlock)
+				.MinDesiredWidth(100)
+				.Text(FText::FromString("Clear"))
+				]
+			]
+		]
+		+ SVerticalBox::Slot()
+		  .FillHeight(1.0f)
+		  .Padding(0.0f, 3.0f, 0.0f, 3.0f)
+		[
+			SNew(SBorder)
+		.Padding(0)
+		.BorderImage(FCoreStyle::Get().GetBrush("ToolPanel.GroupBorder"))
+			[
+				EventsListView.ToSharedRef()
+			]
+		]
+		+ SVerticalBox::Slot()
+		  .AutoHeight()
+		  .Padding(0.0f, 3.0f, 0.0f, 3.0f)
+		[
+			SNew(SHorizontalBox)
+			+ SHorizontalBox::Slot()
+			.MaxWidth(100)
+			[
+				SNew(STextBlock).Text(FText::FromString("Event Type"))
+			]
+			+ SHorizontalBox::Slot()
+			.FillWidth(1.0)
+			[
+				EventTypeComboBox.ToSharedRef()
+			]
+		]
+		+ SVerticalBox::Slot()
+		  .AutoHeight()
+		  .Padding(0.0f, 3.0f, 0.0f, 3.0f)
+		[
+			SuccessHorizontalBox.ToSharedRef()
+		]
+		+ SVerticalBox::Slot()
+		  .AutoHeight()
+		  .Padding(0.0f, 3.0f, 0.0f, 3.0f)
+		[
+			SkuHorizontalBox.ToSharedRef()
+		]
+		+ SVerticalBox::Slot()
+		  .AutoHeight()
+		  .Padding(0.0f, 3.0f, 0.0f, 3.0f)
+		[
+			IsOpenHorizontalBox.ToSharedRef()
+		]
+		+ SVerticalBox::Slot()
+		  .AutoHeight()
+		  .Padding(0, 10.0f, 0, 3.0f)
+		[
+			SNew(SButton)
 		.HAlign(HAlign_Center)
 		.IsEnabled(this, &SElixirEventSimulator::IsSimulating)
 		.OnClicked(this, &SElixirEventSimulator::OnSend)
 		.Content()
-		[
-			SNew(STextBlock)
-			.Text(LOCTEXT("ElixirEventSimulator_Send", "Send"))
-		]
-	];
-	
+			[
+				SNew(STextBlock)
+				.Text(LOCTEXT("ElixirEventSimulator_Send", "Send"))
+			]
+		];
+
 	ChildSlot
 	[
 		SNew(SBorder)
@@ -270,7 +283,6 @@ void SElixirEventSimulator::Construct(const FArguments& InArgs)
 			]
 		]
 	];
-	
 }
 
 void SElixirEventSimulator::Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime)
@@ -278,8 +290,10 @@ void SElixirEventSimulator::Tick(const FGeometry& AllottedGeometry, const double
 	SCompoundWidget::Tick(AllottedGeometry, InCurrentTime, InDeltaTime);
 
 	if (!bSimulating)
+	{
 		return;
-	
+	}
+
 	MessageInterop Msg = ListenToEventBuffer(EventBufferOverlayUi);
 	if (Msg.type != MTEmpty)
 	{
@@ -291,7 +305,7 @@ void SElixirEventSimulator::Tick(const FGeometry& AllottedGeometry, const double
 		TSharedPtr<FElixirEventItem> Item = MakeShareable(new FElixirEventItem);
 		Item->Direction = "Incoming";
 		Item->Name = "Checkout";
-		Item->Payload = FString::Printf(TEXT("\"Sku\": \"%hs\""), Msg.checkout.sku); 
+		Item->Payload = FString::Printf(TEXT("\"Sku\": \"%hs\""), Msg.checkout.sku);
 		Events.Add(Item);
 		EventsListView->RebuildList();
 	}
@@ -310,10 +324,14 @@ bool SElixirEventSimulator::IsSimulating() const
 FReply SElixirEventSimulator::OnStartStop()
 {
 	if (!bSimulating)
+	{
 		StartSimulating();
+	}
 	else
+	{
 		StopSimulating();
-	
+	}
+
 	return FReply::Handled();
 }
 
@@ -321,15 +339,17 @@ FReply SElixirEventSimulator::OnClear()
 {
 	Events.Reset();
 	EventsListView->RebuildList();
-	
+
 	return FReply::Handled();
 }
 
 FReply SElixirEventSimulator::OnSend()
 {
 	if (!bSimulating)
+	{
 		return FReply::Handled();
-	
+	}
+
 	if (SelectedEventType == "CheckoutResult")
 	{
 		SendCheckoutResult(SuccessCheckBox->IsChecked(), SkuEditableTextBox->GetText().ToString());
@@ -337,16 +357,18 @@ FReply SElixirEventSimulator::OnSend()
 	else if (SelectedEventType == "OpenStateChange")
 	{
 		SendOpenStateChange(IsOpenCheckBox->IsChecked());
-	}	
-	
+	}
+
 	return FReply::Handled();
 }
 
 void SElixirEventSimulator::StartSimulating()
 {
 	if (bSimulating)
+	{
 		return;
-	
+	}
+
 	EventBufferOverlayUi = CreateEventBuffer(GetEventBufferOverlayUi());
 	ClearEventBuffer(EventBufferOverlayUi);
 
@@ -367,8 +389,8 @@ void SElixirEventSimulator::StartSimulating()
 		ClearEventBufferError(EventBufferGameSdk);
 		UE_LOG(LogEditorElixir, Error, TEXT("Failed to creaete a GameSdk event buffer (error = '%hs')"), Error2);
 		return;
-	}	
-	
+	}
+
 	bSimulating = true;
 }
 
@@ -390,32 +412,36 @@ bool SElixirEventSimulator::SendCheckoutResult(bool Result, const FString& Sku)
 		return false;
 	}
 
-	const char* SkuChar = TCHAR_TO_ANSI(*Sku);
-	size_t BytesWritten = WriteToEventBufferCheckoutResult(EventBufferGameSdk, Result, SkuChar);
+	size_t BytesWritten = WriteToEventBufferCheckoutResult(EventBufferGameSdk, Result, TCHAR_TO_ANSI(*Sku));
 	if (BytesWritten == 0)
+	{
 		return false;
+	}
 
 	TSharedPtr<FElixirEventItem> Item = MakeShareable(new FElixirEventItem);
 	Item->Direction = "Outgoing";
 	Item->Name = "CheckoutResult";
-	Item->Payload = FString::Printf(TEXT("\"Result\": %s, \"Sku\": \"%s\""), *UKismetStringLibrary::Conv_BoolToString(Result), *Sku); 
+	Item->Payload = FString::Printf(
+		TEXT("\"Result\": %s, \"Sku\": \"%s\""), *UKismetStringLibrary::Conv_BoolToString(Result), *Sku);
 	Events.Add(Item);
 	EventsListView->RebuildList();
 
-	return true;	
+	return true;
 }
 
 bool SElixirEventSimulator::SendOpenStateChange(bool IsOpen)
 {
 	size_t BytesWritten = WriteToEventBufferOpenStateChange(EventBufferGameSdk, IsOpen);
 	if (BytesWritten == 0)
+	{
 		return false;
-	
-	
+	}
+
+
 	TSharedPtr<FElixirEventItem> Item = MakeShareable(new FElixirEventItem);
 	Item->Direction = "Outgoing";
 	Item->Name = "OpenStateChange";
-	Item->Payload = FString::Printf(TEXT("\"IsOpen\": %s"), *UKismetStringLibrary::Conv_BoolToString(IsOpen)); 
+	Item->Payload = FString::Printf(TEXT("\"IsOpen\": %s"), *UKismetStringLibrary::Conv_BoolToString(IsOpen));
 	Events.Add(Item);
 	EventsListView->RebuildList();
 
--- Source/ThirdParty/Raven/include/OverlayMessage.h
+++ Source/ThirdParty/Raven/include/OverlayMessage.h
@@ -1,31 +1,25 @@
 #ifndef ELIXIR_OVERLAY_MESSAGE_H
 #define ELIXIR_OVERLAY_MESSAGE_H
 
-#include <cstdint>
-#include <vector>
-#include <string>
-#include <thread>
-#include <functional>
-#include <memory>
-#include <variant>
 #include "raven_export.h"
-
-struct PShmBuffer_;
-typedef struct PShmBuffer_ PShmBuffer;
-
-struct PError_;
-typedef struct PError_ PError;
-
-struct PShm_;
-typedef struct PShm_ PShm;
 
 namespace elixir::overlay::message
 {
 
-inline auto EVENT_BUFFER_OVERLAY_UI = "EVENT_BUFFER_OVERLAY_UI";
-inline auto EVENT_BUFFER_GAME_SDK = "EVENT_BUFFER_GAME_SDK";
-
 #pragma pack(push, 1)
+
+extern "C" enum MessageType : int32_t
+{
+    MTEmpty = 0,
+    MTToken = 1,
+    MTOpenStateChange = 2,
+    MTCheckout = 3,
+    MTCheckoutResult = 4,
+    MTFeatureFlags = 5,
+    MTLanguage = 6,
+    MTSetVisibility = 7
+};
+
 extern "C" struct RAVEN_EXPORT MTokenInterop
 {
     const char* token;
@@ -62,21 +56,22 @@ extern "C" struct RAVEN_EXPORT MSetVisibilityInterop
     bool newVisibility;
 };
 
+// The following monstrosity disables the pesky 'extern-c-compat' warning that Unreal elevates to error
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wextern-c-compat"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wextern-c-compat"
+#endif
 extern "C" struct RAVEN_EXPORT MEmptyInterop
 {
 };
-
-extern "C" enum RAVEN_EXPORT MessageType : int32_t
-{
-    MTEmpty = 0,
-    MTToken = 1,
-    MTOpenStateChange = 2,
-    MTCheckout = 3,
-    MTCheckoutResult = 4,
-    MTFeatureFlags = 5,
-    MTLanguage = 6,
-    MTSetVisibility = 7
-};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 extern "C" struct RAVEN_EXPORT MessageInterop
 {
@@ -95,170 +90,6 @@ extern "C" struct RAVEN_EXPORT MessageInterop
 };
 #pragma pack(pop)
 
-inline char* ToCharPtr(const std::string& str)
-{
-    char* cstr = new char[str.length() + 1];
-    cstr[str.length()] = 0;
-    std::memcpy(cstr, str.c_str(), str.length());
-    return cstr;
-}
-
-struct MToken
-{
-    MessageType messageType{ MTToken };
-    std::string token;
-
-    template <class T> void pack(T& pack) { pack(messageType, token); }
-    MTokenInterop           ToInterop() { return { ToCharPtr(token) }; };
-    static MToken           FromInterop(const MTokenInterop& message) { return { MTToken, message.token }; }
-};
-
-struct MOpenStateChange
-{
-    MessageType messageType{ MTOpenStateChange };
-    bool        isOpen;
-
-    template <class T> void pack(T& pack) { pack(messageType, isOpen); }
-    MOpenStateChangeInterop ToInterop() { return { isOpen }; }
-    static MOpenStateChange FromInterop(const MOpenStateChangeInterop& message) { return { MTOpenStateChange, message.isOpen }; }
-};
-
-struct MCheckout
-{
-    MessageType messageType{ MTCheckout };
-    std::string sku;
-
-    template <class T> void pack(T& pack) { pack(messageType, sku); }
-    MCheckoutInterop        ToInterop() const { return { ToCharPtr(sku) }; };
-    static MCheckout        FromInterop(const MCheckoutInterop& message) { return { MTCheckout, message.sku }; }
-};
-
-struct MCheckoutResult
-{
-    MessageType messageType{ MTCheckoutResult };
-    bool        success;
-    std::string sku;
-
-    template <class T> void pack(T& pack) { pack(messageType, success, sku); }
-    MCheckoutResultInterop  ToInterop() const { return { success, ToCharPtr(sku) }; };
-    static MCheckoutResult  FromInterop(const MCheckoutResultInterop& message)
-    {
-        return { MTCheckoutResult, message.success, std::string(message.sku) };
-    }
-};
-
-struct MFeatureFlags
-{
-    MessageType messageType{ MTFeatureFlags };
-    std::string featureFlags;
-
-    template <class T> void pack(T& pack) { pack(messageType, featureFlags); }
-    MFeatureFlagsInterop    ToInterop() const { return { ToCharPtr(featureFlags) }; };
-    static MFeatureFlags    FromInterop(const MFeatureFlagsInterop& message) { return { MTFeatureFlags, message.featureFlags }; }
-};
-
-struct MLanguage
-{
-    MessageType messageType{ MTLanguage };
-    std::string language;
-
-    template <class T> void pack(T& pack) { pack(messageType, language); }
-    MLanguageInterop        ToInterop() const { return { ToCharPtr(language) }; };
-    static MLanguage        FromInterop(const MLanguageInterop& message) { return { MTLanguage, message.language }; }
-};
-
-struct MSetVisibility
-{
-    MessageType messageType{ MTSetVisibility };
-    bool        newVisibility;
-
-    template <class T> void pack(T& pack) { pack(messageType, newVisibility); }
-    MSetVisibilityInterop   ToInterop() { return { newVisibility }; }
-    static MSetVisibility   FromInterop(const MSetVisibilityInterop& message) { return { MTSetVisibility, message.newVisibility }; }
-};
-
-struct MEmpty
-{
-    MessageType messageType{ MTEmpty };
-
-    template <class T> void pack(T& pack) { pack(messageType); }
-    MEmptyInterop           ToInterop() { return {}; }
-    static MEmpty           FromInterop(const MEmptyInterop& message) { return { MTEmpty }; }
-};
-
-struct Message
-{
-    MessageType                                                                                                          type;
-    std::variant<MToken, MOpenStateChange, MCheckout, MCheckoutResult, MFeatureFlags, MLanguage, MSetVisibility, MEmpty> data;
-
-    MessageInterop ToInterop()
-    {
-        MessageInterop messageInterop;
-        messageInterop.type = type;
-
-        switch (type)
-        {
-            case MTToken:
-                messageInterop.token = std::get<MToken>(data).ToInterop();
-                break;
-            case MTOpenStateChange:
-                messageInterop.openStateChange = std::get<MOpenStateChange>(data).ToInterop();
-                break;
-            case MTCheckout:
-                messageInterop.checkout = std::get<MCheckout>(data).ToInterop();
-                break;
-            case MTCheckoutResult:
-                messageInterop.checkoutResult = std::get<MCheckoutResult>(data).ToInterop();
-                break;
-            case MTFeatureFlags:
-                messageInterop.featureFlags = std::get<MFeatureFlags>(data).ToInterop();
-                break;
-            case MTLanguage:
-                messageInterop.language = std::get<MLanguage>(data).ToInterop();
-                break;
-            case MTSetVisibility:
-                messageInterop.setVisibility = std::get<MSetVisibility>(data).ToInterop();
-                break;
-            case MTEmpty:
-                messageInterop.empty = std::get<MEmpty>(data).ToInterop();
-                break;
-            default:
-                break;
-        }
-
-        return messageInterop;
-    }
-};
-
-class EventBuffer
-{
-public:
-    explicit EventBuffer(const char* bufferName);
-    ~EventBuffer();
-
-    std::unique_ptr<std::thread> listen(
-        const std::function<bool()>& shouldShutdown, const std::function<void(std::vector<unsigned char>&)>& eventCallback);
-    Message     listenSync();
-    void        clear();
-    size_t      write(const MToken& message);
-    size_t      write(const MOpenStateChange& message);
-    size_t      write(const MCheckout& message);
-    size_t      write(const MCheckoutResult& message);
-    size_t      write(const MFeatureFlags& message);
-    size_t      write(const MLanguage& message);
-    size_t      write(const MSetVisibility& message);
-    size_t      write(const MEmpty& message);
-    const char* getError();
-    void        clearError();
-
-private:
-    std::vector<uint8_t> prependSizeToMessage(std::vector<uint8_t> packedMessage);
-    void listenImpl(const std::function<bool()>& shouldShutdown, const std::function<void(std::vector<unsigned char>&)>& eventCallback);
-    PShmBuffer*            eventBuffer;
-    PError*                eventBufferError;
-    inline static uint32_t instanceCount = 0;
-};
-
 class RAVEN_EXPORT EventBufferInterop
 {
 public:
@@ -272,29 +103,8 @@ public:
     void           clear();
 
 private:
-    EventBuffer* eventBufferImpl;
+    class EventBuffer* eventBufferImpl;
 };
-
-int32_t readMessageLength(PShmBuffer* eventBuffer, int32_t* outLength);
-size_t  getMessageSizeByType(MessageType messageType);
-
-std::vector<unsigned char> packMessage(MToken message);
-std::vector<unsigned char> packMessage(MOpenStateChange message);
-std::vector<unsigned char> packMessage(MCheckout message);
-std::vector<unsigned char> packMessage(MCheckoutResult message);
-std::vector<unsigned char> packMessage(MFeatureFlags message);
-std::vector<unsigned char> packMessage(MLanguage message);
-std::vector<unsigned char> packMessage(MSetVisibility message);
-std::vector<unsigned char> packMessage(MEmpty message);
-
-void unpackMessage(const std::vector<unsigned char>& buffer, MToken& value);
-void unpackMessage(const std::vector<unsigned char>& buffer, MOpenStateChange& value);
-void unpackMessage(const std::vector<unsigned char>& buffer, MCheckout& value);
-void unpackMessage(const std::vector<unsigned char>& buffer, MCheckoutResult& value);
-void unpackMessage(const std::vector<unsigned char>& buffer, MFeatureFlags& value);
-void unpackMessage(const std::vector<unsigned char>& buffer, MLanguage& value);
-void unpackMessage(const std::vector<unsigned char>& buffer, MSetVisibility& value);
-void unpackMessage(const std::vector<unsigned char>& buffer, MEmpty& value);
 
 extern "C" RAVEN_EXPORT EventBufferInterop* CreateEventBuffer(const char* bufferName);
 extern "C" RAVEN_EXPORT void                DestroyEventBuffer(EventBufferInterop* eventBuffer);
@@ -307,9 +117,8 @@ extern "C" RAVEN_EXPORT size_t              WriteToEventBufferCheckout(EventBuff
 extern "C" RAVEN_EXPORT size_t              WriteToEventBufferSetVisibility(EventBufferInterop* eventBuffer, bool newVisibility);
 extern "C" RAVEN_EXPORT size_t              WriteToEventBufferCheckoutResult(EventBufferInterop* eventBuffer, bool result, const char* sku);
 extern "C" RAVEN_EXPORT size_t              WriteToEventBufferOpenStateChange(EventBufferInterop* eventBuffer, bool openState);
-
-extern "C" RAVEN_EXPORT const char* GetEventBufferOverlayUi();
-extern "C" RAVEN_EXPORT const char* GetEventBufferGameSdk();
+extern "C" RAVEN_EXPORT const char*         GetEventBufferOverlayUi();
+extern "C" RAVEN_EXPORT const char*         GetEventBufferGameSdk();
 
 } // namespace elixir::overlay::message
 
